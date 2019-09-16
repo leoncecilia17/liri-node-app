3eea659bb72ba91795cab5fb35b8117bb678a3af
Cecilia Leon <leoncecilia17@gmail.com>
2019-09-16 15:54:37 -0500

    added do this functionality

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
@@ -8,32 +9,44 @@ var moment = require("moment");
 
 // takes user's input and command method 
 var command = process.argv[2];
-var search = process.argv.slice(3);
+var search = process.argv.slice(3).join(" ");
 
-switch (command) {
-    case "concert-this":
-        concertThis(search);
-        break;
+randomInput(command, search);
 
-    case "spotify-this-song":
-        spotifyThis(search);
-        break;
+function randomInput(command, search) {
+    switch (command) {
+        case "concert-this":
+            concertThis(search);
+            break;
 
-    case "movie-this":
-        movieThis(search);
-        break;
+        case "spotify-this-song":
 
-    case "do-what-it-says":
-        doThis();
-        break;
-    default:
-        console.log("Please enter a valid command!");
+            spotifyThis(search);
+            break;
+
+        case "movie-this":
+            movieThis(search);
+            break;
+
+        case "do-what-it-says":
+            doThis();
+            break;
+        default:
+            console.log("Please enter a valid command!");
+    }
 }
 
-function concertThis(artist) {
-    axios.get("https://rest.bandsintown.com/artists" + artist + "/events?app_id=codingbootcamp")
+function concertThis(search) {
+    const queryURL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp"
+    axios.get(queryURL)
         .then(function (response) {
-            console.log("Venue Name: " + response.venue.name);
+            for (var i = 0; i < response.data.length; i++) {
+                var date = response.data[i].datetime;
+                var newDate = date.split(" ");
+                console.log("Name of the Venue: " + response.data[i].venue.name);
+                console.log("Venue Location: " + response.data[i].venue.city);
+                console.log("Date of the Event: " + moment(newDate[i]).format("MM-DD-YYYY"));
+            }
         })
         .catch(function (error) {
             console.log(error);
@@ -41,19 +54,29 @@ function concertThis(artist) {
 }
 
 function spotifyThis() {
-    spotify.search({ type: 'track', query: 'All the Small Things' })
+    if (!search) {
+        search = "The Weekend"; 
+    }
+    else {
+    spotify
+        .search({ type: 'track', query: search })
         .then(function (response) {
-            console.log(response.data[0]);
+            console.log("Artist's Name: ", response.tracks.items[0].album.artists[0].name);
+            console.log("Song: ", response.tracks.items[0].name);
+            console.log("URL: ", response.tracks.items[0].external_urls.spotify);
+            console.log("Album: ", response.tracks.items[0].album.name);
         })
         .catch(function (err) {
             console.log(err);
-        });
+        })
+    }
 }
 
-function movieThis(movie) {
-    axios.get("http://www.omdbapi.com/?t=" + movie + "&apikey=8de1603b&s")
+function movieThis() {
+    axios.get("http://www.omdbapi.com/?t=" + search + "&apikey=8de1603b&s")
         .then(function (response) {
             if (response.data.Title === undefined) {
+                console.log("Please enter the name of a movie. In the meantime, enjoy this bot suggestion!")
                 console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/")
                 console.log("It's on Netflix!")
             }
@@ -80,6 +103,11 @@ function doThis() {
         if (error) {
             return console.log(error);
         }
-        console.log(data);
+        var input = data.split(",");
+        console.log(data); 
+        command = input[0]; 
+        search = input[1]; 
+        randomInput(command, search);
     });
 }
+
